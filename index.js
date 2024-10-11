4adf39165e002104d3cfa5f0c16e71ffa5a6d31c
chefinazia <rachitarora1993@gmail.com>
2024-10-11 20:15:19 +0530

    wrote code for positive test cases

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function stringCalculator(inputStr) {
-    if (inputStr.length === 0) {
-        return 0;
-    }
+  if (inputStr.length === 0) {
+    return 0;
+  }
+  const numArr = inputStr.split(",");
+  let total = 0;
+  numArr.forEach((num) => {
+    total += Number(num);
+  });
+
+  return total;
 }
 
-module.exports = stringCalculator
+module.exports = stringCalculator;
